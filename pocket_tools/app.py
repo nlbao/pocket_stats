b113bab3aeff23c6b565dc65d253471595c18f5d
Bao Nguyen <nlbao95@gmail.com>
2020-07-07 19:57:55 +0800

    Add reading time interactive components.

--- pocket_tools/app.py
+++ pocket_tools/app.py
@@ -1,16 +1,21 @@
 import random
 import plotly
 import pandas as pd
-from typing import List, Dict, Any
+from typing import List, Dict, Tuple, Any
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import plotly.express as px
 
 from data import load_cache, count_words_in_title, get_word_counts, get_reading_time, get_domain_counts
 from data import get_added_time_series, get_archived_time_series, get_language_counts, get_favorite_count
 from constants import DEFAULT_READING_SPEED
+
+
+app = dash.Dash()
+data = load_cache()
 
 
 def plot_two_columns(col0: Any, col1: Any) -> html.Div:
@@ -66,7 +71,7 @@ def word_counts_plot(data: List[Dict]) -> dcc.Graph:
         name='Archived articles',
     ))
     fig.update_layout(
-        title_text='Word Count',  # title of plot
+        title_text='Word Count Distribution',  # title of plot
         xaxis_title_text='Number of words',
         yaxis_title_text='Number of articles',
         barmode='stack'  # The two histograms are drawn on top of another
@@ -74,25 +79,67 @@ def word_counts_plot(data: List[Dict]) -> dcc.Graph:
     return dcc.Graph(id='word-count', figure=fig)
 
 
-def reading_time_plot(data: List[Dict]) -> dcc.Graph:
+# -------------------- Reading time -------------------- #
+def get_reading_time_chart(reading_speed: int) -> go.Figure:
     fig = go.Figure()
     fig.add_trace(go.Histogram(
-        x=get_reading_time(data, filters=[['status', '=', 0]]),  # unread
+        x=get_reading_time(data, reading_speed=reading_speed, filters=[['status', '=', 0]]),  # unread
         name='Unread articles',
     ))
     fig.add_trace(go.Histogram(
-        x=get_reading_time(data, filters=[['status', '=', 1]]),  # archived
+        x=get_reading_time(data, reading_speed=reading_speed, filters=[['status', '=', 1]]),  # archived
         name='Archived articles',
     ))
     fig.update_layout(
-        title_text='Reading Time',  # title of plot
-        xaxis_title_text=f'Estimated reading time (minutes) with reading speed = {DEFAULT_READING_SPEED} wpm',
+        title_text='Reading Time Distribution',  # title of plot
+        xaxis_title_text=f'Estimated reading time (minutes) with reading speed = {reading_speed} wpm',
         yaxis_title_text='Number of articles',
         barmode='stack'  # The two histograms are drawn on top of another
     )
-    return dcc.Graph(id='reading-time', figure=fig)
+    return fig
 
 
+def get_reading_time_needed(reading_speed: int) -> str:
+    total_minutes = int(sum(get_reading_time(data, reading_speed=reading_speed, filters=[['status', '=', 0]])))
+    days, hours, minutes = int(int(total_minutes/24)/60), int(total_minutes/60) % 24, total_minutes % 60
+    ans = ''
+    if days > 0:
+        ans += f' {days} days'
+    if hours > 0:
+        ans += f' {hours} hours'
+    if minutes > 0:
+        ans += f' {minutes} minutes'
+    return html.Div([
+        html.H3(children='Total reading time needed', className='center-text'),
+        html.Div(children=ans, className='center-text highlight'),
+    ])
+
+
+@app.callback(
+    [Output(component_id='reading-time', component_property='figure'),
+     Output(component_id='reading-time-needed', component_property='children')],
+    [Input(component_id='reading-speed', component_property='value')]
+)
+def update_reading_time_components(reading_speed: int) -> Tuple[go.Figure, str]:
+    return (get_reading_time_chart(reading_speed),
+            get_reading_time_needed(reading_speed))
+
+
+def reading_time_plot(data: List[Dict]) -> html.Div:
+    max_reading_speed = DEFAULT_READING_SPEED * 3
+    return html.Div([
+        html.H3(children='Reading speed (wpm)', className='center-text'),
+        dcc.Slider(
+            id='reading-speed',
+            marks={i: str(i) for i in range(100, max_reading_speed, 100)},
+            min=1, max=max_reading_speed, step=10, value=DEFAULT_READING_SPEED
+        ),
+        html.Div(id='reading-time-needed', children=''),
+        dcc.Graph(id='reading-time', figure=go.Figure()),  # figure will be updated by update_reading_time_components()
+    ])
+
+
+# -------------------- Domain -------------------- #
 def domain_counts_plot(data: List[Dict], limit: int = 20) -> dcc.Graph:
     top_pairs = list(get_domain_counts(data).items())  # both unread + archived
     top_pairs.sort(key=lambda p: -p[1])  # sort desc by count
@@ -140,13 +187,10 @@ def favorite_count_plot(data: List[Dict]) -> html.Div:
     res = get_favorite_count(data)
     return html.Div(
         [
-            html.H2(children='Favorite', style={'textAlign': 'center'}),
+            html.H2(children='Favorite', className='center-text'),
             html.H2(
                 children=f"{res['count']} articles ({'%.2f' % (100.0 * res['percent'])} %)",
-                style={
-                    'textAlign': 'center',
-                    'color': 'orange',
-                }
+                className='center-text highlight',
             )
         ],
         className='favorite-div',
@@ -154,8 +198,6 @@ def favorite_count_plot(data: List[Dict]) -> html.Div:
 
 
 if __name__ == '__main__':
-    data = load_cache()
-    app = dash.Dash()
     app.title = "Pocket Analyzer"
     app.layout = html.Div(style={}, children=[
         word_cloud_plot(data),
